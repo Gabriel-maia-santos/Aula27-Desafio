d7b711ca00e555aa99cebc725fc92ee93fcde126
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-06-30 15:01:11 -0300

    Segundo commit

--- Produto.cs
+++ Produto.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Aula27 {
     public class Produto {
@@ -22,7 +25,10 @@ namespace Aula27 {
         }
         private const string PATH = "Database/produto.csv";
 
-
+        /// <summary>
+        /// Cadastra um produto
+        /// </summary>
+        /// <param name="prod">Objeto</param>
         public void Cadastrar (Produto prod) {
             var linha = new string[] {
                 PrepararLinha (prod)
@@ -30,6 +36,36 @@ namespace Aula27 {
             File.AppendAllLines (PATH, linha);
         }
 
+        /// <summary>
+        /// Lê o csv
+        /// </summary>
+        /// <returns>Lista</returns>
+        public List<Produto> Ler(){
+            List<Produto> produtos = new List<Produto>();
+            //tentativa de fazer o desafio:
+
+            var Ler = from p in produtos where Preco > Codigo select produtos;
+
+            //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--==--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+            string[] linhas = File.ReadAllLines(PATH);
+
+            foreach(string linha in linhas){
+                string[] dado = linha.Split(";");
+
+                Produto p = new Produto();
+                p.Codigo = Int32.Parse(dado[0]);
+                p.Nome = dado[1];
+                p.Preco = float.Parse(dado[2]);
+                produtos.Add(p);
+            }
+
+            return produtos;
+        }
+        private string Separar(string _coluna){
+            return _coluna.Split("=")[1];
+        }
+        
         private string PrepararLinha (Produto p) {
             return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
         }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aula27
 {
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Produto p1 = new Produto();
             p1.Codigo = 1;
-            p1.Nome = "Playstation 5";
-            p1.Preco = 4500f;
+            p1.Nome = "Nintendo Switch";
+            p1.Preco = 3499f;
 
             p1.Cadastrar(p1);
+
+            List<Produto> lista = new List<Produto>();
+            lista =  p1.Ler();
+
+            foreach(Produto item in lista){
+                System.Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
 
         }
     }
